3e2e4a17b4da7bb63c8c6558ee42ee36f5c1cda2
Jake Milder <jakemilder@iseinc.biz>
2020-08-15 10:13:48 -0500

    Add player_lives method to disply number of player lives as mini ships

--- astroids/load.py
+++ astroids/load.py
@@ -22,3 +22,13 @@ def distance(point_1=(0,0), point_2=(0, 0)):
     """Returns the distance between two points"""
     return math.sqrt((point_1[0] - point_2[0]) ** 2 + (point_1[1] - point_2[1]) ** 2)
 
+def player_lives(num_icons, batch=None):
+    player_lives = []
+    for i in range(num_icons):
+        new_sprite = pyglet.sprite.Sprite(img=resources.player_image,
+                                            x=785-i*30, y=585, batch=batch)
+
+        new_sprite.scale = 0.5
+        player_lives.append(new_sprite)
+    return player_lives
+
