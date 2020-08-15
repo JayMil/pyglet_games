3a477746454b5163c8f0faaf750b9c6e84575a50
Jake Milder <jakemilder@iseinc.biz>
2020-08-15 09:40:35 -0500

    Add randomly positioned asteroids and ship

--- astroids/astroid.py
+++ astroids/astroid.py
@@ -1,30 +1,20 @@
 import pyglet
-pyglet.resource.path = ['../resources']
-pyglet.resource.reindex()
+
+# local imports
+import resources
+import load
 
 game_window = pyglet.window.Window(800, 600)
 
-player_image = pyglet.resource.image("player.png")
-bullet_image = pyglet.resource.image("bullet.png")
-asteroid_image = pyglet.resource.image("asteroid.png")
-
-#score_label = pyglet.text.Label(text="Score: 0", x=10, y=460)
 score_label = pyglet.text.Label(text="Score: 0", x=10, y=580)
 level_label = pyglet.text.Label(text="My Amazing Game",
                                 x=game_window.width//2, y=580,
                                 anchor_x='center')
 
-def main():
-    center_image(player_image)
-    center_image(bullet_image)
-    center_image(asteroid_image)
+player_ship = pyglet.sprite.Sprite(img=resources.player_image, x=400, y=300)
 
+asteroids = load.asteroids(3, player_ship.position)
 
-
-def center_image(image):
-    """ Sets an image's anchor point to its center """
-    image.anchor_x = image.width // 2
-    image.anchor_y = image.height // 2
 
 @game_window.event
 def on_draw():
@@ -32,7 +22,11 @@ def on_draw():
 
     level_label.draw()
     score_label.draw()
+    player_ship.draw()
 
-if __name__ == '__main__':
-    pyglet.app.run()
-    main()
+    for asteroid in asteroids:
+        asteroid.draw()
+
+
+
+
