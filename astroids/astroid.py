67b7437f71d333347121264993a914285cdb0f36
Jake Milder <jakemilder@iseinc.biz>
2020-08-15 10:07:49 -0500

    Move sprites to batch

--- astroids/astroid.py
+++ astroids/astroid.py
@@ -6,27 +6,22 @@ import load
 
 game_window = pyglet.window.Window(800, 600)
 
-score_label = pyglet.text.Label(text="Score: 0", x=10, y=580)
+main_batch = pyglet.graphics.Batch()
+
+score_label = pyglet.text.Label(text="Score: 0", x=10, y=580, batch=main_batch)
 level_label = pyglet.text.Label(text="My Amazing Game",
                                 x=game_window.width//2, y=580,
-                                anchor_x='center')
+                                anchor_x='center', batch=main_batch)
 
-player_ship = pyglet.sprite.Sprite(img=resources.player_image, x=400, y=300)
+player_ship = pyglet.sprite.Sprite(img=resources.player_image, x=400, y=300, batch=main_batch)
 
-asteroids = load.asteroids(3, player_ship.position)
+asteroids = load.asteroids(3, player_ship.position, batch=main_batch)
 
 
 @game_window.event
 def on_draw():
     game_window.clear()
 
-    level_label.draw()
-    score_label.draw()
-    player_ship.draw()
-
-    for asteroid in asteroids:
-        asteroid.draw()
-
-
+    main_batch.draw()
 
 
--- astroids/load.py
+++ astroids/load.py
@@ -5,7 +5,7 @@ import math
 # local imports
 import resources
 
-def asteroids(num_asteroids, player_position):
+def asteroids(num_asteroids, player_position, batch=None):
     asteroids = []
     for i in range(num_asteroids):
         asteroid_x, asteroid_y = player_position
@@ -13,7 +13,7 @@ def asteroids(num_asteroids, player_position):
             asteroid_x = random.randint(0, 800)
             asteroid_y = random.randint(0, 600)
         new_asteroid = pyglet.sprite.Sprite(img=resources.asteroid_image,
-                                            x=asteroid_x, y=asteroid_y)
+                                            x=asteroid_x, y=asteroid_y, batch=batch)
         new_asteroid.rotation = random.randint(0, 360)
         asteroids.append(new_asteroid)
     return asteroids
